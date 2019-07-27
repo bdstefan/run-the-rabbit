f63bf4eae53a8f03fad0d10419201a6784c30e93
Stefan Boila <dumitru.boila@gmail.com>
2019-07-27 12:55:55 +0300

    Added virtual host property

--- model/consumer.go
+++ model/consumer.go
@@ -6,6 +6,7 @@ type Consumer struct {
 	ID        uint       `gorm:"primary_key" json:"id"`
 	Name      string     `gorm:"type:varchar(100);" valid:"length(3|50)" json:"name"`
 	Host      Host
+	Vhost     string     `gorm:"type:varchar(100);" valid:"length(3|50)" json:"vhost"`
 	Queue     string     `gorm:"type:varchar(100);" valid:"length(3|50)" json:"queue"`
 	Status    bool       `gorm:"type:tinyint(1);" json:"status"`
 	CreatedAt time.Time  `json:"created"`
