8b8b9130128b9b293f433fc50b8f3b825b0eec59
Stefan Boila <dumitru.boila@gmail.com>
2019-07-27 12:46:44 +0300

    Added timestamps for host too

--- model/host.go
+++ model/host.go
@@ -1,6 +1,10 @@
 package model
 
+import "time"
+
 type Host struct {
 	ID        uint       `gorm:"primary_key" json:"id"`
 	Name      string     `gorm:"type:varchar(100);" valid:"length(3|50)" json:"name"`
+	CreatedAt time.Time  `json:"created"`
+	UpdatedAt time.Time  `json:"updated"`
 }
